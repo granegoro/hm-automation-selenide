57fb7eaad544346932b65ebdd7cfcef141a5a2bb
Егор Старшелюк <granegoro@gmail.com>
2022-10-06 18:34:07 +0300

    Test suite complete, error added, build should fail

--- src/test/java/CardDeliveryServiceTest.java
+++ src/test/java/CardDeliveryServiceTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -16,30 +17,192 @@ public class CardDeliveryServiceTest {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
+    String generateDateInPAst(int days) {
+        return LocalDate.now().minusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @BeforeEach
     void setup() {
         open("http://localhost:9999/");
     }
 
+    String clearAll = Keys.chord(Keys.SHIFT, Keys.HOME) + Keys.DELETE;
+
     @Test
-    public void shouldSucceedWithValidData() {
+    public void shouldPassWithValidData() {
         String date = generateDate(3);
 
-        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=city] input").setValue("МоскваR");
+        $("[data-test-id=date] input").setValue(clearAll);
         $("[data-test-id=date] input").setValue(date);
         $("[data-test-id=name] input").setValue("Жора Жорович");
         $("[data-test-id=phone] input").setValue("+79999999999");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-        Duration.ofSeconds(15);
-        $("[data-test-id=notification]")..shouldBe(visible);
+        $$("button .spin");
+        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15));
 
+    }
 
+    @Test
+    public void shouldPassWithDashInName() {
+        String date = generateDate(3);
 
-        /*$(By.name("user.name")).setValue("johny");
-        $("#submit").click();
-        $(".loading_progress").should(disappear); // Waits until element disappears
-        $("#username").shouldHave(text("Hello, Johny!")); // Waits until element gets text*/
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").setValue(clearAll);
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=name] input").setValue("Жо-ра Жо-рович");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $$("button .spin");
+        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15));
+
+    }
+
+    @Test
+    public void shouldNotPassWithInvalidCity() {
+        String date = generateDate(3);
+
+        $("[data-test-id=city] input").setValue("абвгд");
+        $("[data-test-id=date] input").setValue(clearAll);
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=name] input").setValue("Жора Жорович");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=city].input_invalid .input__sub").shouldHave(text("Доставка в выбранный город недоступна"));
+
+    }
+
+    @Test
+    public void shouldNotPassWithBlankCity() {
+        String date = generateDate(3);
+
+        $("[data-test-id=date] input").setValue(clearAll);
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=name] input").setValue("Жора Жорович");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=city].input_invalid .input__sub").shouldHave(text("Поле обязательно для заполнения"));
+
+    }
+
+    @Test
+    public void shouldNotPassWithInvalidDate() {
+        String date = generateDateInPAst(3);
+
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").setValue(clearAll);
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=name] input").setValue("Мистер");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=date] .input_invalid .input__sub").shouldHave(text("Заказ на выбранную дату невозможен"));
+
+    }
+
+    @Test
+    public void shouldNotPassWithCurrentDate() {
+        String date = generateDate(0);
+
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").setValue(clearAll);
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=name] input").setValue("Мистер");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=date] .input_invalid .input__sub").shouldHave(text("Заказ на выбранную дату невозможен"));
+
+    }
+
+    @Test
+    public void shouldNotPassWithBlankDate() {
+        String date = generateDate(0);
+
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").setValue(clearAll);
+        $("[data-test-id=name] input").setValue("Мистер");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=date] .input_invalid .input__sub").shouldHave(text("Неверно введена дата"));
+
+    }
+
+    @Test
+    public void shouldNotPassWithInvalidName() {
+        String date = generateDate(3);
+
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").setValue(clearAll);
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=name] input").setValue("Lars");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+
+    }
+
+    @Test
+    public void shouldNotPassWithBlankName() {
+        String date = generateDate(3);
+
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").setValue(clearAll);
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(text("Поле обязательно для заполнения"));
+
+    }
+
+    @Test
+    public void shouldNotPassWithInvalidPhone() {
+        String date = generateDate(3);
+
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").setValue(clearAll);
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=name] input").setValue("Мистер");
+        $("[data-test-id=phone] input").setValue("999999999");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+
+    }
+
+    @Test
+    public void shouldNotPassWithBlankPhone() {
+        String date = generateDate(3);
+
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").setValue(clearAll);
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=name] input").setValue("Мистер");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(text("Поле обязательно для заполнения"));
+
+    }
+
+    @Test
+    public void shouldNotPassWithoutAgreement() {
+        String date = generateDate(3);
+
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").setValue(clearAll);
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=name] input").setValue("Жора Жорович");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=agreement].input_invalid .checkbox__text").shouldHave(text("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+
     }
 
 
